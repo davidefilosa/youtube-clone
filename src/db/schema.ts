dfa61849b9378fc8e1cf6ddcd5097e4c071e5370
Davide Filosa <davideclaypool@gmail.com!>
2025-02-18 15:10:34 +0100

    api path

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -9,12 +9,12 @@ export const usersTable = pgTable(
   "users",
   {
     id: uuid("id").primaryKey().defaultRandom(),
-    clerId: text("clerk_id").unique().notNull(),
+    clerkId: text("clerk_id").unique().notNull(),
     name: text().notNull(),
 
     imageUrl: text("image_url").notNull(),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
   },
-  (t) => [uniqueIndex("clerk_id_index").on(t.clerId)]
+  (t) => [uniqueIndex("clerk_id_index").on(t.clerkId)]
 );
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/",
-  "/api/webhooks/clerk(.*)",
+  "/api/users/webhook(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
