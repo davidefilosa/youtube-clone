ab9adf5f6ad6e6bd75c436788e595d8c45cac57f
Davide Filosa <davideclaypool@gmail.com!>
2025-02-22 10:37:15 +0100

    force dinamyc

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -3,6 +3,8 @@ import { StudioView } from "@/modules/studio/view/studio-view";
 import { HydrateClient, trpc } from "@/trpc/server";
 import React from "react";
 
+export const dynamic = "force-dynamic";
+
 const StudioPage = async () => {
   void trpc.studio.getMany.prefetchInfinite({ limit: DEFAULT_LIMIT });
   return (
