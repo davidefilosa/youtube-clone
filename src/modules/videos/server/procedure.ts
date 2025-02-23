90b4a9f4569ed06c93ab4e8f9a8878f1571b1b86
Davide Filosa <davideclaypool@gmail.com!>
2025-02-23 08:06:52 +0100

    subtitle

--- src/modules/videos/server/procedure.ts
+++ src/modules/videos/server/procedure.ts
@@ -16,6 +16,11 @@ export const videosRouter = createTRPCRouter({
         passthrough: userId,
         playback_policy: ["public"],
         mp4_support: "standard",
+        input: [
+          {
+            generated_subtitles: [{ language_code: "en", name: "English" }],
+          },
+        ],
       },
       cors_origin: "*", // in a real app, this should be the domain of your app
     });
